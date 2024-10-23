978552c1e9de27f60a44b80849033e2ef65ddba4
Lucas Candiani <lucascandianisouza@gmail.com>
2024-10-23 11:04:48 -0300

    reducing frequency modes from dartsnet

--- train.py
+++ train.py
@@ -92,7 +92,8 @@ def log_alphas(model, writer, step):
                 
                 for i, alpha in enumerate(alphas):
                    primitive = PRIMITIVES[i]
-                   writer.add_scalar(f'{name}_alpha_{i}_{primitive}', alpha.item(), step)
+                   writer.add_scalar(f'{name}_alpha_{i}_{primitive}', alpha[j].item(), step)
+
 
 
 def evaluate(model, criterion, dataloader, device, writer):
